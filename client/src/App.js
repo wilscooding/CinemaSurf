427f3348f6a7f617328bf81e235d4dfd95502b35
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-07-11 12:38:24 -0400

    added app.js routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import Player from './pages/Player';
+import TvShow from './pages/TvShow';
+import CinemaSurf from './pages/CinemaSurf';
+import MoviePage from './pages/MoviePage';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/login' element={<LoginPage/>}/>
+        <Route exact path='/signup' element={<SignUpPage/>}/>
+        <Route exact path='/player' element={<Player/>}/>
+        <Route exact path='/tv' element={<TvShow/>}/>
+        <Route exact path='/' element={<CinemaSurf/>}/>
+        <Route exact path='/movie' element={<MoviePage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
