fa2d70406e054490f2b6f4f81938509d3f94e834
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-07-12 13:46:33 -0400

    added Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import styled from 'styled-components'
 
-const Header = () => {
+const Header = (props) => {
+  const navigate = useNavigate()
   return (
-    <div>Header</div>
+    <HeaderContainer>
+      <div className='logo'>
+        <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668265433/NetflixApp/2560px-Netflix_2015_logo.svg_rbicwl_knwp6f.png'
+        alt='no internet connection'
+        />
+      </div>
+      <button onClick={()=>navigate(props.login ? '/login' : '/signup')}>
+        {props.login ? 'Log In' : 'Sing In'}
+
+      </button>
+    </HeaderContainer>
   )
 }
+
+const HeaderContainer = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0 4rem;
+.logo{
+  img{
+    height: 3rem;
+    cursor: pointer;
+  }
+}
+button{
+  padding: 0.5rem 1rem;
+  background-color: red;
+  border: none;
+  cursor: pointer;
+  color: white;
+  border-radius: 0.2rem;
+  font-weight: bolder;
+  font-size: 1.5rem;
+}
+
+`
 
 export default Header
