1d578064105639b807a3b2e745d1a38566593ea4
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-07-13 09:04:10 -0400

    Login Page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import BackgroundImage from "../components/BackgroundImage";
+import styled from "styled-components";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+	return (
+		<Wrapper>
+			<BackgroundImage />
+			<div className="loginContent">
+				<Header />
+				<div className="form-wrapper">
+					<div className="form">
+						<div className="title">
+							<h1>login</h1>
+						</div>
+						<div className="container">
+							<input type="text" placeholder="email" />
+							<input type="password" placeholder="password" />
+							<button>Login</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	);
+};
+const Wrapper = styled.div`
+	position: relative;
+	.loginContent {
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: rgba(0, 0, 0, 0.79);
+		height: 100vh;
+		width: 100vw;
+		grid-template-columns: 15vh 85vh;
+		.form-wrapper {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			gap: 2rem;
+			flex-direction: column;
+			height: 85vh;
+		}
+		.form {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			gap: 2rem;
+			background-color: #000000b0;
+			border-radius: 0.4rem;
+			padding: 2rem;
+			color: white;
+			height: 70vh;
+		}
+		.container {
+			display: flex;
+			flex-direction: column;
+			gap: 2rem;
+			input {
+				border-radius: 0.4rem;
+				padding: 0.5rem 1rem;
+				width: 25rem;
+				height: 3.4rem;
+				outline: none;
+			}
+			button {
+				padding: 0.5rem;
+				background-color: red;
+				border: none;
+				cursor: pointer;
+				color: white;
+				border-radius: 0.4rem;
+				height: 3.4rem;
+				font-weight: bolder;
+				font-size: 1.05rem;
+			}
+		}
+	}
+`;
 
-export default LoginPage
+export default LoginPage;
