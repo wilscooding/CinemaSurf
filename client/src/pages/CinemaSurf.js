01ce7dc8396671689f3688e2003beeb825be4f0d
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-07-20 14:11:06 -0400

    CinemaSurf home page

--- client/src/pages/CinemaSurf.js
+++ client/src/pages/CinemaSurf.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CinemaSurf = () => {
-  return (
-    <div>CinemaSurf</div>
-  )
-}
+	const [isScrolled, setIsScrolled] = useState(false);
 
-export default CinemaSurf
+	window.onscroll = () => {
+		setIsScrolled(window.pageYOffset === 0 ? false : true);
+		return () => (window.onscroll = null);
+	};
+	return (
+		<div className="background">
+			<img
+				src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+				alt="background img"
+			/>
+      <img
+				src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+				alt="background img"
+			/>
+      <img
+				src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+				alt="background img"
+			/>
+      <img
+				src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+				alt="background img"
+			/>
+		</div>
+	);
+};
+
+export default CinemaSurf;
