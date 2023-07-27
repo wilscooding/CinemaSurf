e9543b1acd1c9ed32f09a35b03a05dcbc4110188
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-07-27 08:15:45 -0300

    fix CSS card component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -37,23 +37,30 @@ const Card = () => {
 						/>
 					</div>
 					<div className="info-container">
-						<h3 className="movieName" onClick={()=> navigate("/player")}>Avengers</h3>
+						<h3
+							className="movieName"
+							onClick={() => navigate("/player")}
+						>
+							Avengers
+						</h3>
 						<div className="icons">
-							<IoPlayCircleSharp title="play" onClick={()=> navigate("/player")} />
-							<RiThumbUpFill title="like"/>
-							<RiThumbDownFill title="dislike"/>
-							<BsCheck title="remove from list"/>
-							<AiOutlinePlus title="add to my list"/>
+							<IoPlayCircleSharp
+								title="play"
+								onClick={() => navigate("/player")}
+							/>
+							<RiThumbUpFill title="like" />
+							<RiThumbDownFill title="dislike" />
+							<BsCheck title="remove from list" />
+							<AiOutlinePlus title="add to my list" />
 						</div>
 						<div className="info">
-							<BiChevronDown title="more info"/>
+							<BiChevronDown title="more info" />
 						</div>
 					</div>
 					<div className="genre">
 						<ul>
 							<li>Action & Adventure</li>
 							<li>Science Fiction</li>
-							
 						</ul>
 					</div>
 				</div>
@@ -64,7 +71,94 @@ const Card = () => {
 };
 
 const CardContainer = styled.div`
+	margin-top: 1rem;
 	max-width: 230px;
-	background-color: red;
+	width: 230px;
+	height: 100%;
+	cursor: pointer;
+	position: relative;
+	/* background-color: red; */
+	img {
+		border-radius: 0.2rem;
+		width: 100%;
+		height: 100%;
+		z-index: 10;
+	}
+	.hover {
+		z-index: 99;
+		height: max-content;
+		width: 20rem;
+		position: absolute;
+		top: -18vh;
+		left: 0;
+		border-radius: 0.2rem;
+		border: 0.1rem solid gray;
+		background-color: #181818;
+		transition: 0.3s ease-out;
+		.image-video-wrapper {
+			position: relative;
+			height: 140px;
+			img {
+				width: 100%;
+				height: 150px;
+				object-fit: cover;
+				border-radius: 0.3rem;
+				top: 0;
+				z-index: 4;
+				position: absolute;
+			}
+			video {
+				width: 100%;
+				height: 150px;
+				object-fit: cover;
+				border-radius: 0.3rem;
+				top: 0;
+				z-index: 4;
+				position: absolute;
+			}
+		}
+		.info-container {
+			display: flex;
+			flex-direction: column;
+			padding: 1rem;
+			gap: 0.5rem;
+			.movieName {
+				color: white;
+			}
+		}
+		.icons {
+			display: flex;
+			justify-content: space-between;
+			.controls {
+				display: flex;
+				gap: 0.5rem;
+			}
+		}
+		svg {
+			color: white;
+			border: 0.1rem solid white;
+			border-radius: 50%;
+			font-size: 1.5rem;
+			cursor: pointer;
+			transition: 0.3s ease-in-out;
+			&:hover {
+				background-color: #b8b8b8;
+			}
+		}
+		.genre {
+			display: flex;
+			color: white;
+			ul {
+				display: flex;
+				gap: 1rem;
+				li {
+					padding-right: 0.7rem;
+					&:first-of-type {
+						list-style-type: none;
+					}
+				}
+			}
+		}
+	}
 `;
 export default Card;
