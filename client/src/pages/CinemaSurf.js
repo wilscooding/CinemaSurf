d4aaa8faf042ec4c19981dd5e5703720bf531f6b
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-07-31 11:32:37 -0300

    added movie genres

--- client/src/pages/CinemaSurf.js
+++ client/src/pages/CinemaSurf.js
@@ -1,155 +1,30 @@
-// import React, { useState, useEffect } from "react";
-// import styled from "styled-components";
-// import { AiOutlineInfoCircle } from "react-icons/ai";
-// import { FaPlay } from "react-icons/fa";
-// import { useNavigate } from "react-router-dom";
-
-// import TopNav from "../components/TopNav";
-// import Card from "../components/Card";
-
-// const CinemaSurf = () => {
-// 	const [isScrolled, setIsScrolled] = useState(false);
-
-// 	const navigate = useNavigate();
-
-// 	window.onscroll = () => {
-// 		setIsScrolled(window.scrollY === 0 ? false : true);
-// 		return () => (window.onscroll = null);
-// 	};
-
-// 	// useEffect(() => {
-// 	// 	const handleScroll = () => {
-// 	// 		setIsScrolled(window.scrollY > 0);
-// 	// 	};
-
-// 	// 	// Add event listener
-// 	// 	window.addEventListener("scroll", handleScroll);
-
-// 	// 	// Cleanup function
-// 	// 	return () => {
-// 	// 		window.removeEventListener("scroll", handleScroll);
-// 	// 	};
-// 	// }, []);
-// 	return (
-// 		<HeroContainer>
-// 			<div className="background">
-// 				<TopNav isScrolled={isScrolled} /> vcc
-// 				<img
-// 					className="background-image"
-// 					src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
-// 					alt="background img"
-// 				/>
-// 					<div className="container">
-// 					<div className="title">
-// 						<h1>Cinema Surf</h1>
-// 						<p>A virtual movie theater in your pocket.</p>
-// 					</div>
-// 					<div className="buttons">
-// 						<button
-// 							onClick={() => navigate("/player")}
-// 							className="playBtn"
-// 						>
-// 							Play
-// 						</button>
-// 						<button className="moreBtn">More</button>
-// 					</div>
-// 				</div>
-// 			</div>
-// 			<Card />
-// 		</HeroContainer>
-// 	);
-// };
-
-// const HeroContainer = styled.div`
-// 	.background {
-// 		position: relative;
-// 		.background-image {
-// 			filter: brightness(40%);
-// 		}
-// 		img {
-// 			height: 70vh;
-// 			width: 100%;
-// 		}
-// 	}
-// 	.container {
-// 		position: absolute;
-// 		bottom: 1rem;
-// 		.title {
-// 			h1 {
-// 				margin-left: 5rem;
-// 				text-transform: uppercase;
-// 				font-size: 73px;
-// 				background: -webkit-linear-gradient(#eee, rgb(128, 13, 13));
-// 				-webkit-background-clip: text;
-// 				-webkit-text-fill-color: transparent;
-// 			}
-// 			p {
-// 				margin-bottom: -50px;
-// 				width: 640px;
-// 				margin-left: 5rem;
-// 				font-family: "lexend Deca", sans-serif;
-// 				color: white;
-// 			}
-// 		}
-// 		.buttons {
-// 			display: flex;
-// 			margin: 5rem;
-// 			gap: 2rem;
-// 		}
-// 		.playBtn {
-// 			display: flex;
-// 			align-items: center;
-// 			justify-content: center;
-// 			color: blue;
-// 			border-radius: 1rem;
-// 			font-size: 1.4rem;
-// 			gap: 1rem;
-// 			padding: 0.9rem;
-// 			padding-left: 2rem;
-// 			padding-right: 2.4rem;
-// 			border: none;
-// 			cursor: pointer;
-// 		}
-// 		.moreBtn {
-// 			display: flex;
-// 			align-items: center;
-// 			justify-content: center;
-// 			color: white;
-// 			background-color: black;
-// 			border-radius: 1rem;
-// 			font-size: 1.4rem;
-// 			gap: 1rem;
-// 			padding: 0.9rem;
-// 			padding-left: 2rem;
-// 			padding-right: 2.4rem;
-// 			border: 0.1rem solid white;
-// 			cursor: pointer;
-// 		}
-// 	}
-// `;
-
-// export default CinemaSurf;
-
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import {useDispatch } from "react-redux"
 
 import TopNav from "../components/TopNav";
 import Card from "../components/Card";
+import { getGenres } from "../store";
 
 const CinemaSurf = () => {
 	const [isScrolled, setIsScrolled] = useState(false);
 	const navigate = useNavigate();
+
+	const dispatch = useDispatch()
+
+	useEffect(()=>{
+		dispatch(getGenres())
+	},[])
 
 	useEffect(() => {
 		const handleScroll = () => {
 			setIsScrolled(window.scrollY > 0);
 		};
 
-		// Add event listener
+
 		window.addEventListener("scroll", handleScroll);
 
-		// Cleanup function
 		return () => {
 			window.removeEventListener("scroll", handleScroll);
 		};
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,19 +1,40 @@
-import {configureStore, createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import {
+	configureStore,
+	createAsyncThunk,
+	createSlice,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+import { MY_API_KEY, TMDB_BASE_URL } from "../utils/constant";
 
 const initialState = {
-  movies: [],
-  generesLoaded: false,
-  genres: []
-}
+	movies: [],
+	generesLoaded: false,
+	genres: [],
+};
+
+export const getGenres = createAsyncThunk("cinemaSurf/genres", async () => {
+	const {
+		data: { genres },
+	} = await axios.get(
+		`${TMDB_BASE_URL}/genre/movie/list?api_key=${MY_API_KEY}`
+	);
+	console.log(genres);
+	return genres;
+});
 
 const CinemaSurfSlice = createSlice({
-  name: "CinemaSurf",
-  initialState,
-  extraReducers: (builder)=>{}
-})
+	name: "CinemaSurf",
+	initialState,
+	extraReducers: (builder) => {
+		builder.addCase(getGenres.fulfilled, (state, action) => {
+			state.genres = action.payload;
+			state.generesLoaded = true;
+		});
+	},
+});
 
 export const store = configureStore({
-  reducer: {
-    cinemaSurf: CinemaSurfSlice.reducer
-  }
-})
+	reducer: {
+		cinemaSurf: CinemaSurfSlice.reducer,
+	},
+});
