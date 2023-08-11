6f929f20f2ba0a5af3a5d6d7dd885ae5f937de0a
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-08-11 14:00:51 -0300

    movie rows

--- client/src/components/MovieSlider.js
+++ client/src/components/MovieSlider.js
@@ -1,16 +1,43 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import styled from "styled-components";
 
-const MovieSlider = ({data, title}) => {
-  return (
-    <div className='slider'>
-      {
-        data.map((movie, index) =>{
-          return <Card movieData={movie} index={index} key={movie.id}/>
-        })
-      }
-    </div>
-  )
-}
+const MovieSlider = ({ data, title }) => {
+	return (
+		<Container>
+			<h1>{title}</h1>
+			<div className="wrapper">
+				<div className="slider">
+					{data.map((movie, index) => {
+						return (
+							<Card
+								movieData={movie}
+								index={index}
+								key={movie.id}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</Container>
+	);
+};
 
-export default MovieSlider
+const Container = styled.div`
+	gap: 0.7rem;
+	position: relative;
+	padding: 2rem 0;
+	h1 {
+		margin-left: 50px;
+		font-size: 25px;
+		font-family: "Franklin Gothic Medium", "Arial Narrow", Arial,
+			sans-serif;
+	}
+  .wrapper{
+    .slider{
+      display: flex;
+    }
+  }
+`;
+
+export default MovieSlider;
